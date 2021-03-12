2d5b51bd84d60519b9083d15a3cf560247c9035f
Amit Karn <amit.karn98@gmail.com>
2021-03-13 00:03:17 +0545

    added new source folder

---
+++ source/navbar.php
@@ -0,0 +1,23 @@
+<div class="nav">
+    <input type="checkbox" id="nav-check">
+    <div class="nav-header">
+        <div class="nav-title">
+        RIP & ROAR
+        </div>
+    </div>
+    <div class="nav-btn">
+        <label for="nav-check">
+        <span></span>
+        <span></span>
+        <span></span>
+        </label>
+    </div>
+    
+    <div class="nav-links">
+        <a href="#" target="_blank">Home</a>
+        <a href="#" target="_blank">Contact Us</a>
+    </div>
+
+    
+</div>
+      
